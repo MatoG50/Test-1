bc873d85fe7e157d9b6d7afef2916a986f57c9dd
MatoG50 <matog50@gmail.com>
2021-04-20 11:59:51 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// 1.
+// Array with 50 numbers
+const numbersArr = [];
+
+for (let x = 1; x < 51; x++) {
+  numbersArr.push(x);
+}
+console.log(numbersArr);
+
+// Divisible by 2
+
+const divBy2 = numbersArr.filter(function (num) {
+  return num % 2 === 0;
+});
+
+console.log(divBy2);
+
+// Total
+
+const total = divBy2.reduce((acc, num) => acc + num, 0);
+console.log(total);
+
+// 2.
+
+const buttonRandom = document.querySelector('.btn-random');
+
+const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+buttonRandom.addEventListener('click', function () {
+  const randomName = [];
+
+  for (let i = 0; i < 7; i++) {
+    randomName.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+  }
+  console.log(randomName.join(''));
+});
+
+// 3.
+
+// alert(divBy2);
